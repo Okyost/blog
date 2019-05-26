9abbb0876a272d47212397cae21263cdb7bdcf7a
Okyost <Gr1nya24@yandex.ru>
2019-05-26 14:54:38 +0300

    Попытка 2, комит 1

---
+++ app/routes.py
@@ -0,0 +1,28 @@
+from app import app
+from flask import render_template
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Sasha'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
